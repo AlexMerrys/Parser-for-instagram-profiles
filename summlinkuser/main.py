350611e3c4d53a465319396e974f3c28d9b1a669
Alexandr Cherepovskiy <alexcherepovskiy0@gmail.com>
2025-09-17 00:23:34 +0300

    Update main.py

--- summlinkuser/main.py
+++ summlinkuser/main.py
@@ -1,7 +1,9 @@
+# Превращение упоминаний @аккаунт и в конкертные ссылки
+
 import pandas as pd
 
 # Загружаем таблицу с @usernames
-df = pd.read_excel("usernames_raw.xlsx")  # Заменить на имя твоего файла
+df = pd.read_excel("usernames_raw.xlsx")
 column = "Mentions"  # Название колонки с @юзернеймами
 
 # Удаляем @ и добавляем к ссылке
@@ -10,4 +12,5 @@ df["Instagram Link"] = "https://www.instagram.com/" + df[column].str.replace("@"
 # Сохраняем результат
 df.to_excel("instagram_links_converted.xlsx", index=False)
 
-print("✅ Ссылки сохранены в 'instagram_links_converted.xlsx'")
+print("Ссылки сохранены в 'instagram_links_converted.xlsx'")
+
